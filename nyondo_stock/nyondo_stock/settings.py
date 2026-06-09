aa1f05405e9cf9dac9475e54e1695e03fc1356c1
allennakalema06 <allennakalema06@gmail.com>
2026-06-09 15:46:07 +0300

    Prepare project for Render deployment

--- nyondo_stock/nyondo_stock/settings.py
+++ nyondo_stock/nyondo_stock/settings.py
@@ -57,11 +57,9 @@ INSTALLED_APPS = [
 ]
 LOGIN_URL = 'login'
 MIDDLEWARE = [
-
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
